0a88888f0096e7c0b20b922995e165d91da4dc05
Entreprenerd <alex@entreprenerd.xyz>
2024-07-17 15:18:47 +0200

    chore: docs

--- src/MacroERC20.sol
+++ src/MacroERC20.sol
@@ -6,6 +6,7 @@ contract MacroERC20 is ERC20 {
 
     constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {}
     
+    /// @dev Replaces the first parameter of AbiEncodedData with the address of the caller
     function _replaceCaller(bytes memory abiEncodedData) internal view returns (bytes memory) {
       // Make a copy of the data
       bytes memory copy = abiEncodedData;
@@ -19,6 +20,8 @@ contract MacroERC20 is ERC20 {
       return copy;
     }
 
+    /// @notice Given a target and abi encoded data, we replace the first parameter to be the address of the caller
+    ///   And we then call the target
     function callWithForwardedData(address target, bytes memory encodedData) public returns (bool, bytes memory) {
       bytes memory dataToCallWith = _replaceCaller(encodedData);
       (bool success, bytes memory returnData) = target.call(dataToCallWith);
@@ -32,6 +35,8 @@ contract MacroERC20 is ERC20 {
       bool requireSuccess;
     }
 
+    /// @notice Given macro data perform a multicall to any target
+    /// @dev We replace the first parameter with the address of the msg.sender, always
     function doTheMacro(MacroData[] memory macroData) external {
       uint256 length = macroData.length;
       for(uint256 i; i < length; ++i) {
